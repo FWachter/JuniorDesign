82741719cee098a8381719f00bca6650882313f5
FWachter <wachterfreddy@gmail.com>
2016-11-27 19:13:30 -0500

    Added flipping functions

--- DemoCode/DemoCode.ino
+++ DemoCode/DemoCode.ino
@@ -9,20 +9,15 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
-#define MOTOR1_PWM 11
-#define MOTOR2_PWM 3
-#define MOTOR3_PWM 6
-#define MOTOR4_PWM 5
 #define SERVO1_PWM 10
-#define SERVO2_PWM 9
+#define SERVO2_PWM 11
+#define SERVO1_START 120
+#define SERVO1_MAX 0
+#define SERVO2_START 0
+#define SERVO2_MAX 100
 
-#define SERVO1_FULLSPEED_C 135
-#define SERVO1_FULLSPEED_CC 45
-#define SERVO1_MAXTURN 180
-#define SERVO1_180DEG 315
-#define SERVO1_STOP 90
-
-#define IR_SENSOR_PIN 2
+#define IR_PIN_LONG 2
+#define IR_PIN_SHORT 3
 
 #define US_TRIGGER_PIN  5  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define US_ECHO_PIN     6  // Arduino pin tied to echo pin on the ultrasonic sensor.
@@ -31,7 +26,7 @@
 #define ACCEL_SAMPLERATE_DELAY_MS 100
 #define ORIENT_Z_FLIPPED 30
 
-Servo servo1; // setup servo
+Servo servo1, servo2; // setup servos
 NewPing sonar(US_TRIGGER_PIN, US_ECHO_PIN, US_MAX_DISTANCE); // setup ultrasonic sensor
 Adafruit_BNO055 bno = Adafruit_BNO055(55); // setup accelerometer
 
@@ -41,9 +36,11 @@ void setup() {
 
   // Initialize Servo
   servo1.attach(SERVO1_PWM);
+  servo2.attach(SERVO2_PWM);
 
   // Initialize IR Sensor
-  pinMode(IR_SENSOR_PIN, INPUT);
+  pinMode(IR_PIN_LONG, INPUT);
+  pinMode(IR_PIN_SHORT, INPUT);
 
   // Initilaize Accelerometer
   if (!bno.begin()) {
@@ -52,16 +49,13 @@ void setup() {
   }
   bno.setExtCrystalUse(true);
 
+  resetMotors();
+
 }
 
 void loop() {
 
-  bool flipped = isFlipped();
-  if (flipped) {
-    if (getIRSensorData()) {
-      Serial.println("Starting flipping maneuver");
-    }
-  }
+  controlLoop();
 
 }
 
@@ -69,12 +63,12 @@ void loop() {
 // ---------- EXAMPLE FUNCTIONS ----------
 // ---------------------------------------
 
-void readIRSensorExample() {
+void readIRSensorExample(int pin) {
 // Programmer: Frederick Wachter - wachterfreddy@gmail.com
 // Date Created: November 26th, 2016
 // Purpose: Example of reading from the IR sensor
 
-  bool value = getIRSensorData();
+  bool value = getIRSensorData(pin);
   Serial.println(value);
   
 }
@@ -105,28 +99,73 @@ void readUltrasonicExample() {
   
 }
 
-void moveServoExample() {
-// Programmer: Frederick Wachter - wachterfreddy@gmail.com
-// Date Created: November 25th, 2016
-// Purpose: Example of using the moveServo() function
- 
-  moveServo(servo1, 180);
-  Serial.println("Position 1");
-  delay(1000);
-  moveServo(servo1, -180);
-  Serial.println("Position 2");
-  delay(1000);
-  
-}
 
 // -------------------------------------
 // ---------- LOGIC FUNCTIONS ----------
 // -------------------------------------
 
+void flipOver() {
+// Programmer: Frederick Wachter - wachterfreddy@gmail.com
+// Date Created: November 27th, 2016
+// Purpose: Run through sequence to flip over crab
+
+  moveServo(servo2, SERVO2_MAX);
+  delay(2000);
+  moveServo(servo1, SERVO1_MAX);
+  delay(200);
+  moveServo(servo2, SERVO2_START);
+  delay(2000);
+  resetMotors();
+  
+}
+
+void flipOverFast() {
+// Programmer: Frederick Wachter - wachterfreddy@gmail.com
+// Date Created: November 27th, 2016
+// Purpose: Run through sequence to flip over crab fast
+
+  moveServo(servo2, SERVO2_MAX);
+  delay(500);
+  moveServo(servo1, SERVO1_MAX);
+  delay(100);
+  moveServo(servo2, SERVO2_START);
+  delay(1000);
+  resetMotors();
+  
+}
+
+void controlLoop() {
+// Programmer: Frederick Wachter - wachterfreddy@gmail.com
+// Date Created: November 25th, 2016
+// Purpose: Control loop for flipping
+
+  if (isFlipped()) {
+    if (getIRSensorData(IR_PIN_SHORT)) {
+      flipOverFast();
+    } else if (getIRSensorData(IR_PIN_LONG)) {
+      bool flipFast = 0;
+      unsigned long time_start = millis();
+      
+      while ((millis() - time_start) < 500) {
+        if (getIRSensorData(IR_PIN_SHORT)) {
+          flipFast = 1;
+          break;
+        }
+      }
+
+      if (flipFast) {
+        flipOverFast();
+      } else {
+        flipOver();
+      }
+    }
+  }
+}
+
 bool isFlipped() {
 // Programmer: Frederick Wachter - wachterfreddy@gmail.com
 // Date Created: November 25th, 2016
-// Purpose: Example of reading from the accelerometer
+// Purpose: Check if the horseshoe crab is flipped upside down
 
   bool is_flipped = 0;
 
@@ -139,16 +178,28 @@ bool isFlipped() {
   
 }
 
+void resetMotors() {
+// Programmer: Frederick Wachter - wachterfreddy@gmail.com
+// Date Created: November 27th, 2016
+// Purpose: Reset motor positions
+
+  servo1.write(SERVO1_START);
+  servo2.write(SERVO2_START);
+  delay(1000);
+  
+}
+
+
 // --------------------------------------
 // ---------- SENSOR FUNCTIONS ----------
 // --------------------------------------
 
-bool getIRSensorData() {
+bool getIRSensorData(int pin) {
 // Programmer: Frederick Wachter - wachterfreddy@gmail.com
 // Date Created: November 26th, 2016
 // Purpose: Get data from the IR sensor
 
-  bool value = !digitalRead(IR_SENSOR_PIN);
+  bool value = !digitalRead(pin);
   return value;
 
 }
@@ -180,30 +231,32 @@ unsigned int getUltrasonicData(NewPing &sonar) {
  
 }
 
-void moveServo(Servo &servo, long int turn_degrees) {
+void moveServo(Servo &servo, int turn_degrees) {
 // Programmer: Frederick Wachter - wachterfreddy@gmail.com
 // Date Created: November 25th, 2016
 // Purpose: Move the provided servo to a provided position
 
-  // Initilaize Variables
-  int wait_time;
+  turn_degrees = max(0, turn_degrees);
 
-  // Calculations
-  turn_degrees = min(max(-SERVO1_MAXTURN, turn_degrees), SERVO1_MAXTURN); // make sure the provided turn degree is within +-180 degrees
-  wait_time = abs((turn_degrees*SERVO1_180DEG)/180); // calculate the amount of time the servo needs to be moving to get to position
-
-  // Set Direction of Servo
-  if (turn_degrees > 0) {
-    // turn the servo clockwise
-    servo.write(SERVO1_FULLSPEED_C);
-  } else {
-    // turn the servo counter clockwise
-    servo.write(SERVO1_FULLSPEED_CC);
-  }
-
-  // Wait for Servo to be at Desired Position then Stop
-  delay(wait_time);
-  servo.write(SERVO1_STOP);
+  servo.write(turn_degrees);
   
 }
 
+
+// -----------------------------------------
+// ---------- DEPICATED FUNCTIONS ----------
+// -----------------------------------------
+
+void moveServoExample() {
+// Programmer: Frederick Wachter - wachterfreddy@gmail.com
+// Date Created: November 25th, 2016
+// Purpose: Example of using the moveServo() function
+ 
+  moveServo(servo1, 180);
+  Serial.println("Position 1");
+  delay(1000);
+  moveServo(servo1, -180);
+  Serial.println("Position 2");
+  delay(1000);
+  
+}
